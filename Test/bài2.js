a41c8c241c4560c29eae9a44de298142298915dc
dat200495 <hahahahafaker@gmail.com>
2022-11-13 23:59:17 +0700

    test cuoi

--- Test/bài2.js
+++ Test/bài2.js
@@ -1,11 +1,13 @@
 function numberOneTriangle(){
     let a = Number(document.getElementById("canh").value);
+    let result = "";
     if (a<=10) {
         for (let i =1; i <= a; i++){
             for (let j=1; j<=a; j++) {
                 if (j<=i){
                     result += ("*");
-                };
+                } else {
+                    result +=" ";}
             };
         };
         result += "\n";
