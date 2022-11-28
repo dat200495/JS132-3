0307d6f8c3b95710b040c6468404b503feac81ed
dat200495 <hahahahafaker@gmail.com>
2022-11-28 16:15:20 +0700

    sua bai1js

--- ThiLai/Bai1/bai1.js
+++ ThiLai/Bai1/bai1.js
@@ -1,8 +1,9 @@
 function findOppostieNumber (){
     let a = Number(document.getElementById("thamso1").value);
     let b = Number(document.getElementById("thamso2").value);
-    if (a >20 || a%2 ==! 0) {
-        alert("N phải nhỏ hơn 20 và là số dương");
+    if (a >20|| a<4 || a%2 ==! 0) {
+        alert("N phải nhỏ hơn 20, lớn hơn 4 và là số dương");
+        b = "Không có kết quả";
     } else { 
         if (b>a) {
             alert("Số đối phải nhỏ hơn N");
